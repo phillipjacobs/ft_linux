d72269c78f7ffaf3ae7593a2df84e46530a2f89b
phjacobs7ag <phjacobs@student.42.fr>
2017-09-10 17:11:26 +0200

    fixed

--- setup/steps/step_4_3_adjusting.sh
+++ setup/steps/step_4_3_adjusting.sh
@@ -26,52 +26,6 @@ sed 's/tools/usr/' /tools/lib/libstdc++.la > /usr/lib/libstdc++.la
 ln -sv bash /bin/sh
 ln -sv /proc/self/mounts /etc/mtab
 
-# echo "root:x:0:0:root:/root:/bin/bash
-# bin:x:1:1:bin:/dev/null:/bin/false
-# daemon:x:6:6:Daemon User:/dev/null:/bin/false
-# messagebus:x:18:18:D-Bus Message Daemon User:/var/run/dbus:/bin/false
-# systemd-bus-proxy:x:72:72:systemd Bus Proxy:/:/bin/false
-# systemd-journal-gateway:x:73:73:systemd Journal Gateway:/:/bin/false
-# systemd-journal-remote:x:74:74:systemd Journal Remote:/:/bin/false
-# systemd-journal-upload:x:75:75:systemd Journal Upload:/:/bin/false
-# systemd-network:x:76:76:systemd Network Management:/:/bin/false
-# systemd-resolve:x:77:77:systemd Resolver:/:/bin/false
-# systemd-timesync:x:78:78:systemd Time Synchronization:/:/bin/false
-# systemd-coredump:x:79:79:systemd Core Dumper:/:/bin/false
-# nobody:x:99:99:Unprivileged User:/dev/null:/bin/false" > /etc/passwd
-
-
-# echo "root:x:0:
-# bin:x:1:daemon
-# sys:x:2:
-# kmem:x:3:
-# tape:x:4:
-# tty:x:5:
-# daemon:x:6:
-# floppy:x:7:
-# disk:x:8:
-# lp:x:9:
-# dialout:x:10:
-# audio:x:11:
-# video:x:12:
-# utmp:x:13:
-# usb:x:14:
-# cdrom:x:15:
-# adm:x:16:
-# messagebus:x:18:
-# systemd-journal:x:23:
-# input:x:24:
-# mail:x:34:
-# systemd-bus-proxy:x:72:
-# systemd-journal-gateway:x:73:
-# systemd-journal-remote:x:74:
-# systemd-journal-upload:x:75:
-# systemd-network:x:76:
-# systemd-resolve:x:77:
-# systemd-timesync:x:78:
-# systemd-coredump:x:79:
-# nogroup:x:99:
-# users:x:999:" > /etc/group
 
 echo "
 root:x:0:0:root:/root:/bin/bash
@@ -117,4 +71,7 @@ touch /var/log/{btmp,lastlog,faillog,wtmp}
 chgrp -v utmp /var/log/lastlog
 chmod -v 664  /var/log/lastlog
 chmod -v 600  /var/log/btmp
+
+
+
 
