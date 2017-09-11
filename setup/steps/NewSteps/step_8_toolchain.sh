d2a2e7e71bfa6ea2bf791e08d60465368b0dc6a6
phjacobs7ag <phjacobs@student.42.fr>
2017-09-11 18:07:08 +0200

    update

--- setup/steps/NewSteps/step_8_toolchain.sh
+++ setup/steps/NewSteps/step_8_toolchain.sh
@@ -6,7 +6,7 @@ if [ "$(echo $LFS)" = "" ]; then
 	printf "\033[31m[ {✗}ERROR  ] LFS variable not present. Check step 1\033[0m\n"
 	exit 1
 else
-	printf "\033[32m[ {✓}SUCCESS ] LFS variable was set successfully\033[0m\n"
+	printf "\033[32m[ {✓ }SUCCESS ] LFS variable was set successfully\033[0m\n"
 fi 
 
 
@@ -19,12 +19,12 @@ status=0
 #
 # main
 #
-printf "\033[36m[ {✓}SUCCESS ]\033[0m Step 1 - building the toolchain\n"
+printf "\033[36m[ {✓ }SUCCESS ]\033[0m Step 1 - building the toolchain\n"
 
 #
 # Preparing the build
 #
-printf "\033[36m[ {✓}SUCCESS ]\033[0m checking for file requirements...\n"
+printf "\033[36m[ {✓ }SUCCESS ]\033[0m checking for file requirements...\n"
 
 for line in `cat $dir_toolchain_config`
 do
@@ -38,24 +38,24 @@ do
 done
 
 if [ $status -eq 1 ]; then
-	printf "\033[31m[ {✗}ERROR ]\033[0m failed to retrieve all toolchain files scripts\n"
+	printf "\033[31m[ {✗ }ERROR ]\033[0m failed to retrieve all toolchain files scripts\n"
 	exit $status
 fi
 
 # TODO add an exit for log dir
 mkdir -p $dir_toolchain_log													&& \
-	printf "\033[32m[ {✓}SUCCESS ]\033[0m log directory created\n"				|| \
-	printf "\033[31m[ {✓}SUCCESS ]\033[0m failed to create log directory\n"
+	printf "\033[32m[ {✓ }SUCCESS ]\033[0m log directory created\n"				|| \
+	printf "\033[31m[ {✓ }SUCCESS ]\033[0m failed to create log directory\n"
 
 #
 # Build the system
 #
-printf "\033[36m[ {✓}SUCCESS ]\033[0m ready to build toolchain\n"
+printf "\033[36m[ {✓ }SUCCESS ]\033[0m ready to build toolchain\n"
 
 for line in `cat $dir_toolchain_config`
 do
 	build_file=$(echo $dir_toolchain/$line.sh)
-	printf "\033[36m[ {✓}SUCCESS ]\033[0m running %-30s" "$build_file"
+	printf "\033[36m[ {✓ }SUCCESS ]\033[0m running %-30s" "$build_file"
 	sh $build_file "$(echo $LFS)/sources" "$dir_toolchain_log"
 	status=$?
 	if [ $status -eq 0 ]; then	
