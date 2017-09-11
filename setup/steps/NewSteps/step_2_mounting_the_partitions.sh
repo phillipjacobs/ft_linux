ba0a0bbdbbef393ce1a26a65140e4a0dade7a679
phjacobs7ag <phjacobs@student.42.fr>
2017-09-11 14:34:13 +0200

    updated scripts

--- setup/steps/NewSteps/step_2_mounting_the_partitions.sh
+++ setup/steps/NewSteps/step_2_mounting_the_partitions.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+echo $LFS
+
 mkdir -pv $LFS							|| exit
 mount -v -t ext4 /dev/sdb1 $LFS			|| exit
 
 mkdir -pv $LFS/boot						|| exit
-mount -v -t ext4 /dev/sdb2 $LFS/boot	|| exit
+mount -v -t ext4 /dev/sdb3 $LFS/boot	|| exit
 
-/sbin/swapon -v /dev/sdb3
+/sbin/swapon -v /dev/sdb4
