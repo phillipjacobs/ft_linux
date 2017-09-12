d8efd016cf44ab36c9d3160894c94a1bea7d67a7
phjacobs7ag <phjacobs@student.42.fr>
2017-09-12 13:36:09 +0200

    updated scripts

--- setup/steps/NewSteps/build/system/readline-7.0.sh
+++ setup/steps/NewSteps/build/system/readline-7.0.sh
@@ -19,15 +19,21 @@ setup(){
 build(){
 	sed -i '/MV.*old/d' Makefile.in													|| return
 	sed -i '/{OLDSUFF}/c:' support/shlib-install									|| return
-	./configure --prefix=/usr	\
-		--disable-static		\
-		--docdir=/usr/share/doc/readline-6.3										|| return
-	make SHLIB_LIBS=-lncurses														|| return
-	make SHLIB_LIBS=-lncurses install												|| return
+	
+	./configure --prefix=/usr    \
+		--disable-static \
+		--docdir=/usr/share/doc/readline-7.0										|| return
+	
+	make SHLIB_LIBS="-L/tools/lib -lncursesw"										|| return
+	make SHLIB_LIBS="-L/tools/lib -lncurses" install								|| return
+
+	# Now move the dynamic libraries to a more appropriate
+	# location and fix up some symbolic links:
 	mv -v /usr/lib/lib{readline,history}.so.* /lib									|| return
 	ln -sfv ../../lib/$(readlink /usr/lib/libreadline.so) /usr/lib/libreadline.so	|| return
 	ln -sfv ../../lib/$(readlink /usr/lib/libhistory.so ) /usr/lib/libhistory.so	|| return
-	install -v -m644 doc/*.{ps,pdf,html,dvi} /usr/share/doc/readline-6.3			|| return
+	#If desired, install the documentation:
+	install -v -m644 doc/*.{ps,pdf,html,dvi} /usr/share/doc/readline-7.0			|| return
 }
 
 teardown(){
