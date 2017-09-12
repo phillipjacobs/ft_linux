40feaf59acc033b214c68bac6b251d1707889d90
phjacobs7ag <phjacobs@student.42.fr>
2017-09-12 13:16:00 +0200

    updated scripts before testing starts

--- setup/steps/NewSteps/build/system/glibc-2.26.sh
+++ setup/steps/NewSteps/build/system/glibc-2.26.sh
@@ -60,43 +60,43 @@ build(){
 
 	# Fix the generated Makefile to skip an uneeded sanity check that
 	# fails in the LFS partial environment:
-	sed '/test-installation/s@$(PERL)@echo not running@' -i ../Makefile
+	sed '/test-installation/s@$(PERL)@echo not running@' -i ../Makefile		|| return
 
 	make install															|| return
 
 	# Install the configuration file and runtime directory for nscd :
-	cp -v ../nscd/nscd.conf /etc/nscd.conf
-	mkdir -pv /var/cache/nscd
+	cp -v ../nscd/nscd.conf /etc/nscd.conf									|| return
+	mkdir -pv /var/cache/nscd												|| return
 
 
 	# The following instructions will install the minimum set of
 	# locales necessary for the optimal coverage of tests:
-	mkdir -pv /usr/lib/locale
-	localedef -i cs_CZ -f UTF-8 cs_CZ.UTF-8
-	localedef -i de_DE -f ISO-8859-1 de_DE
-	localedef -i de_DE@euro -f ISO-8859-15 de_DE@euro
-	localedef -i de_DE -f UTF-8 de_DE.UTF-8
-	localedef -i en_GB -f UTF-8 en_GB.UTF-8
-	localedef -i en_HK -f ISO-8859-1 en_HK
-	localedef -i en_PH -f ISO-8859-1 en_PH
-	localedef -i en_US -f ISO-8859-1 en_US
-	localedef -i en_US -f UTF-8 en_US.UTF-8
-	localedef -i es_MX -f ISO-8859-1 es_MX
-	localedef -i fa_IR -f UTF-8 fa_IR
-	localedef -i fr_FR -f ISO-8859-1 fr_FR
-	localedef -i fr_FR@euro -f ISO-8859-15 fr_FR@euro
-	localedef -i fr_FR -f UTF-8 fr_FR.UTF-8
-	localedef -i it_IT -f ISO-8859-1 it_IT
-	localedef -i it_IT -f UTF-8 it_IT.UTF-8
-	localedef -i ja_JP -f EUC-JP ja_JP
-	localedef -i ru_RU -f KOI8-R ru_RU.KOI8-R
-	localedef -i ru_RU -f UTF-8 ru_RU.UTF-8
-	localedef -i tr_TR -f UTF-8 tr_TR.UTF-8
-	localedef -i zh_CN -f GB18030 zh_CN.GB18030
+	mkdir -pv /usr/lib/locale												|| return
+	localedef -i cs_CZ -f UTF-8 cs_CZ.UTF-8									|| return
+	localedef -i de_DE -f ISO-8859-1 de_DE									|| return
+	localedef -i de_DE@euro -f ISO-8859-15 de_DE@euro						|| return
+	localedef -i de_DE -f UTF-8 de_DE.UTF-8									|| return
+	localedef -i en_GB -f UTF-8 en_GB.UTF-8									|| return
+	localedef -i en_HK -f ISO-8859-1 en_HK									|| return
+	localedef -i en_PH -f ISO-8859-1 en_PH									|| return
+	localedef -i en_US -f ISO-8859-1 en_US									|| return
+	localedef -i en_US -f UTF-8 en_US.UTF-8									|| return
+	localedef -i es_MX -f ISO-8859-1 es_MX									|| return
+	localedef -i fa_IR -f UTF-8 fa_IR										|| return
+	localedef -i fr_FR -f ISO-8859-1 fr_FR									|| return
+	localedef -i fr_FR@euro -f ISO-8859-15 fr_FR@euro						|| return
+	localedef -i fr_FR -f UTF-8 fr_FR.UTF-8									|| return
+	localedef -i it_IT -f ISO-8859-1 it_IT									|| return
+	localedef -i it_IT -f UTF-8 it_IT.UTF-8									|| return
+	localedef -i ja_JP -f EUC-JP ja_JP										|| return
+	localedef -i ru_RU -f KOI8-R ru_RU.KOI8-R								|| return
+	localedef -i ru_RU -f UTF-8 ru_RU.UTF-8									|| return
+	localedef -i tr_TR -f UTF-8 tr_TR.UTF-8									|| return
+	localedef -i zh_CN -f GB18030 zh_CN.GB18030								|| return
 
 	# In addition, install the locale for your own country,
 	# language and character set.
-	make localedata/install-locales
+	make localedata/install-locales											|| return
 
 	echo "
 # Begin /etc/nsswitch.conf
@@ -110,25 +110,24 @@ services: files
 ethers: files
 rpc: files
 # End /etc/nsswitch.conf
-" > /etc/nsswitch.conf
+" > /etc/nsswitch.conf														|| return
 
 
 	# Install and set up the time zone data with the following:
-	tar -xf ../../tzdata2017b.tar.gz
+	tar -xf ../../tzdata2017b.tar.gz										|| return
 	ZONEINFO=/usr/share/zoneinfo
-	mkdir -pv $ZONEINFO/{posix,right}
+	mkdir -pv $ZONEINFO/{posix,right}										|| return
 	for tz in etcetera southamerica northamerica europe africa antarctica  \
 	          asia australasia backward pacificnew systemv; do
 	    zic -L /dev/null   -d $ZONEINFO       -y "sh yearistype.sh" ${tz}
 	    zic -L /dev/null   -d $ZONEINFO/posix -y "sh yearistype.sh" ${tz}
 	    zic -L leapseconds -d $ZONEINFO/right -y "sh yearistype.sh" ${tz}
 	done
-	cp -v zone.tab zone1970.tab iso3166.tab $ZONEINFO
+	cp -v zone.tab zone1970.tab iso3166.tab $ZONEINFO						|| return
 	zic -d $ZONEINFO -p America/New_York
 	unset ZONEINFO
 
-	cp -v /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-
+	cp -v /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime			|| return
 
 	# Configuring the Dynamic Loader
 	echo "
@@ -136,28 +135,16 @@ rpc: files
 /usr/local/lib
 /opt/lib
 
-" > /etc/ld.so.conf
+" > /etc/ld.so.conf															|| return
 
 	echo "
 # Add an include directory
 include /etc/ld.so.conf.d/*.conf
 
-" >> /etc/ld.so.conf
+" >> /etc/ld.so.conf														|| return
 
-	mkdir -pv /etc/ld.so.conf.d
+	mkdir -pv /etc/ld.so.conf.d												|| return
 
-
-	# Adjusting the toolchain
-	mv -v /tools/bin/{ld,ld-old}
-	mv -v /tools/$(uname -m)-pc-linux-gnu/bin/{ld,ld-old}
-	mv -v /tools/bin/{ld-new,ld}
-	ln -sv /tools/bin/ld /tools/$(uname -m)-pc-linux-gnu/bin/ld
-
-
-	gcc -dumpspecs | sed -e 's@/tools@@g'                   \
-	    -e '/\*startfile_prefix_spec:/{n;s@.*@/usr/lib/ @}' \
-	    -e '/\*cpp:/{n;s@$@ -isystem /usr/include@}' >      \
-	    `dirname $(gcc --print-libgcc-file-name)`/specs
 
 	# Testing
 	# echo 'int main(){}' > dummy.c
@@ -193,17 +180,18 @@ include /etc/ld.so.conf.d/*.conf
 teardown(){
 	cd $base_dir
 	rm -rfv $pkg_name
+
 	# Adjusting the toolchain
-	mv -v /tools/bin/{ld,ld-old}
-	mv -v /tools/$(uname -m)-pc-linux-gnu/bin/{ld,ld-old}
-	mv -v /tools/bin/{ld-new,ld}
-	ln -sv /tools/bin/ld /tools/$(uname -m)-pc-linux-gnu/bin/ld
+	mv -v /tools/bin/{ld,ld-old}											|| return
+	mv -v /tools/$(uname -m)-pc-linux-gnu/bin/{ld,ld-old}					|| return
+	mv -v /tools/bin/{ld-new,ld}											|| return
+	ln -sv /tools/bin/ld /tools/$(uname -m)-pc-linux-gnu/bin/ld				|| return
 
 
 	gcc -dumpspecs | sed -e 's@/tools@@g'                   \
 	    -e '/\*startfile_prefix_spec:/{n;s@.*@/usr/lib/ @}' \
 	    -e '/\*cpp:/{n;s@$@ -isystem /usr/include@}' >      \
-	    `dirname $(gcc --print-libgcc-file-name)`/specs
+	    `dirname $(gcc --print-libgcc-file-name)`/specs						|| return
 }
 
 # Internal process
--- setup/steps/step_4_2_entering_chroot.sh
+++
@@ -1,6 +0,0 @@
-chroot "$LFS" /tools/bin/env -i \
-	HOME=/root                  \
-	TERM="$TERM"                \
-	PS1='\u:\w\$ '              \
-	PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin \
-	/tools/bin/bash --login +h
