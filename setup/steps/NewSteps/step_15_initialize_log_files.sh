ae48329dcc6066c8d919b03ac04060dd5a21631c
phjacobs7ag <phjacobs@student.42.fr>
2017-09-12 09:54:41 +0200

    added step 15 script

---
+++ setup/steps/NewSteps/step_15_initialize_log_files.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# user enable to run script :: root
+
+if [ "$(echo $LFS)" = "" ]; then
+	printf "\033[31m[ { ✗ }ERROR  ] LFS variable not present. Check step 1\033[0m\n"
+	exit 1
+else
+	printf "\033[32m[ { ✓ }SUCCESS ] LFS variable was set successfully\033[0m\n"
+fi 
+
+# Initialize the log files and give them proper permissions:
+touch /var/log/{btmp,lastlog,faillog,wtmp}
+chgrp -v utmp /var/log/lastlog
+chmod -v 664  /var/log/lastlog
+chmod -v 600  /var/log/btmp
+
+printf "\033[32m[ { ✓ }SUCCESS ] Now time for the second most scary part.\033[0m\n"
