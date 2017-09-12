dfed571dafa60c122050cbbf9df225329cd3cfb9
phjacobs7ag <phjacobs@student.42.fr>
2017-09-12 15:45:17 +0200

    added some scripts: shadow 4.5

--- setup/steps/NewSteps/build/system/shadow-4.5.sh
+++ setup/steps/NewSteps/build/system/shadow-4.5.sh
@@ -16,20 +16,37 @@ setup(){
 }
 
 build(){
-	sed -i 's/groups$(EXEEXT) //' src/Makefile.in						|| return
+	sed -i 's/groups$(EXEEXT) //' src/Makefile.in							|| return
 	find man -name Makefile.in -exec sed -i 's/groups\.1 / /'   {} \;
 	find man -name Makefile.in -exec sed -i 's/getspnam\.3 / /' {} \;
 	find man -name Makefile.in -exec sed -i 's/passwd\.5 / /'   {} \;
-	sed -i -e 's@#ENCRYPT_METHOD DES@ENCRYPT_METHOD SHA512@'	\
-		       -e 's@/var/spool/mail@/var/mail@' etc/login.defs			|| return
-	sed -i 's/1000/999/' etc/useradd									|| return
-	./configure --sysconfdir=/etc --with-group-name-max-length=32		|| return
-	make																|| return
-	make install														|| return
-	mv -v /usr/bin/passwd /bin											|| return
-	pwconv																|| return
-	grpconv																|| return
-	sed -i 's/yes/no/' /etc/default/useradd								|| return
+	sed -i -e 's@#ENCRYPT_METHOD DES@ENCRYPT_METHOD SHA512@' \
+		-e 's@/var/spool/mail@/var/mail@' etc/login.defs					|| return
+
+	# Shadow with Cracklib support
+	sed -i 's@DICTPATH.*@DICTPATH\t/lib/cracklib/pw_dict@' etc/login.defs	|| continue
+
+	# Make a minor change to make the default useradd
+	# consistent with the LFS groups file
+	sed -i 's/1000/999/' etc/useradd										|| return
+	
+
+	./configure --sysconfdir=/etc --with-group-name-max-length=32			|| return
+	
+	make																	|| return
+	make install															|| return
+	
+	mv -v /usr/bin/passwd /bin												|| return
+	
+	# To enable shadowed passwords, run the following command:
+	pwconv																	|| return
+	
+	# To enable shadowed group passwords, run
+	grpconv																	|| return
+	
+	sed -i 's/yes/no/' /etc/default/useradd									|| return
+	
+	#  Setting the root password
 	passwd root
 }
 
